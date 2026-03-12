7f8bb3fe27716d5ee5d344a0e093749aa38fdf60
Oluwaseun Gideon <seungideon73@gmail.com>
2026-03-11 21:06:16 -0700

    made a whole lot of changes both in home&shop html and css

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 document.querySelector('.btn.primary').addEventListener('click', function(event) {
     alert('Redirecting to quote request form...');
 });
+
+const dropdownBtn = document.getElementById('dropdownBtn');
+const dropdownContent = document.getElementById('dropdownContent');
+
+if (dropdownBtn && dropdownContent) {
+    dropdownBtn.addEventListener('click', function(event) {
+        event.stopPropagation();
+        dropdownContent.classList.toggle('show');
+    });
+
+    window.addEventListener('click', function(event) {
+        if (!event.target.matches('.dropbtn')) {
+            if (dropdownContent.classList.contains('show')) {
+                dropdownContent.classList.remove('show');
+            }
+        }
+    });
+}
